66ab10810c03be80b6e08a977460ab33f6f7c3ca
mabeliris <mabelle7623@gmail.com>
2024-02-29 01:18:03 -0300

    Agregar paginaci{on y ordenamiento de los resultados de la petición

--- src/main/java/com/Fleet/Management/demo/repository/TaxiRepository.java
+++ src/main/java/com/Fleet/Management/demo/repository/TaxiRepository.java
@@ -1,11 +1,17 @@
 package com.Fleet.Management.demo.repository;
 
 import com.Fleet.Management.demo.model.Taxi;
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.util.List;
 
-public interface TaxiRepository extends JpaRepository<Taxi, Long> {
+//import java.util.List;
+
+public interface TaxiRepository extends PagingAndSortingRepository<Taxi, Long> {
+
 
 
 }
--- src/main/java/com/Fleet/Management/demo/service/TaxiServices.java
+++ src/main/java/com/Fleet/Management/demo/service/TaxiServices.java
@@ -3,9 +3,14 @@ package com.Fleet.Management.demo.service;
 import com.Fleet.Management.demo.model.Taxi;
 import com.Fleet.Management.demo.repository.TaxiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
+
+
+
+//import java.util.List;
 @Service
 public class TaxiServices {
     private final TaxiRepository taxiRepository;
@@ -15,7 +20,13 @@ public class TaxiServices {
         this.taxiRepository= taxiRepository;
     }
 
-    public List<Taxi> getAllTaxis(){
-        return taxiRepository.findAll();
+    public Page<Taxi> getAllTaxis(Pageable pageable){
+
+        return taxiRepository.findAll(pageable);
     }
 }
+
+/*Page<Product> allProducts = productRepository.findAll(firstPageWithTwoElements);
+
+List<Product> allTenDollarProducts =
+        productRepository.findAllByPrice(10, secondPageWithFiveElements);*/
